9d952f6c695841e747107fa2ce2a3075b814554b
Abhi Raj <ar958587@gmail.com>
2026-01-01 16:48:03 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,10 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be populated by the Firebase tooling.
-export const firebaseConfig = {"apiKey":"your-api-key","authDomain":"your-auth-domain","projectId":"your-project-id","storageBucket":"your-storage-bucket","messagingSenderId":"your-messaging-sender-id","appId":"your-app-id"};
+export const firebaseConfig = {
+  "projectId": "studio-5734454762-8f6d7",
+  "appId": "1:329161090010:web:f3902a56584274c7acd6e3",
+  "apiKey": "AIzaSyA-lmnlQFAtlookAoUfIjrgrnxstezvY_U",
+  "authDomain": "studio-5734454762-8f6d7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "329161090010"
+};
